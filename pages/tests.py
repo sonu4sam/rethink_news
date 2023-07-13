2fca6cb951506c7e854f3992fa238d8db8c14f35
sonu4sam <sonu4sam@outlook.com>
2023-07-12 23:20:03 -0500

    buggy

--- pages/tests.py
+++ pages/tests.py
@@ -2,12 +2,13 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from users.views import SignUpView
+
 class HomePageTest(TestCase):
     
     def test_home_page_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        
         
     def test_view_url_by_name(self):
         response = self.client.get(reverse('home'))
@@ -30,13 +31,12 @@ class SignupPageTests(TestCase):
     def test_view_url_by_name(self):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'signup.html')
         
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('signup'))
+        print(reverse('signup'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup.html')
-        
         
     def test_signup_form(self):
         new_user = get_user_model().objects.create_user(
