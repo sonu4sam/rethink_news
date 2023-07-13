db4c35a62a7c07153dd036d97523e1b68de1ba33
sonu4sam <sonu4sam@outlook.com>
2023-07-12 23:24:14 -0500

    redirect bugfix

--- pages/tests.py
+++ pages/tests.py
@@ -26,7 +26,7 @@ class SignupPageTests(TestCase):
     
     def test_signup_page_status_code(self):
         response = self.client.get('/users/signup')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 301)
         
     def test_view_url_by_name(self):
         response = self.client.get(reverse('signup'))
